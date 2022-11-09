ea10cd68a5df5f40ef36f595be6f16cc9ca3d073
Ari Caldeira <ari.caldeira@tauga.com.br>
2022-11-09 19:11:41 -0300

    [IMP] Mapas de console manuais

--- cria_mapa_console.sh
+++ cria_mapa_console.sh
@@ -7,9 +7,11 @@ echo "ckbcomp -model abnt -layout nativo -variant original > /usr/share/kbd/keym
 echo "ckbcomp -model abnt -layout nativo -variant abnt2 > /usr/share/kbd/keymaps/i386/qwerty/abnt2.map"
 
 # ckbcomp -model abnt -layout nativo -variant nativo > /usr/share/kbd/keymaps/i386/qwerty/nativo.map
-cp nativo.map /usr/share/kbd/keymaps/i386/qwerty/nativo.map
-ckbcomp -model abnt -layout nativo -variant original > /usr/share/kbd/keymaps/i386/qwerty/original.map
-ckbcomp -model abnt -layout nativo -variant abnt2 > /usr/share/kbd/keymaps/i386/qwerty/abnt2.map
+cp nativo.map /usr/share/kbd/keymaps/i386/qwerty/
+#ckbcomp -model abnt -layout nativo -variant original > /usr/share/kbd/keymaps/i386/qwerty/original.map
+cp original.map /usr/share/kbd/keymaps/i386/qwerty/
+#ckbcomp -model abnt -layout nativo -variant abnt2 > /usr/share/kbd/keymaps/i386/qwerty/abnt2.map
+cp abnt2.map /usr/share/kbd/keymaps/i386/qwerty/
 
 echo
 echo "Mapa de teclado do console atualizado com sucesso!"
